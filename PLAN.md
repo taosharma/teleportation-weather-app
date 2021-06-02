d2f54815016be5747ddff9e5ef27c267aff17054
Tao Sharma <tao@schoolofcode.co.uk>
2021-06-02 14:20:21 +0100

    first commit

---
+++ PLAN.md
@@ -0,0 +1,17 @@
+# Weather Teleportation App
+
+## User Flow
+
+See header
+Input box for city
+Add to list button - Makes API call - Gets back data - Adds to list state
+Displays city in input box and temperature in list - Updates list to reflect list state
+
+## Component Tree
+
+- App component - List state lives, API calls
+- Header component
+- Input component
+- AddToListButton component
+- ListItem component
+- List component
--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const API_KEY = "0a9936f40ff53d039d2bf5a4c916c04a";
+
+async function getCityTemperature(cityName) {
+  const response = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+  );
+  const data = await response.json();
+  return { temperature: data.main.temp, city: data.name };
+}
+
+const mockTemps = [
+  { temperature: "20", city: "London" },
+  { temperature: "30", city: "Havana" },
+];
 
 function App() {
+  const [temperatureList, setTemperatureList] = useState(mockTemps);
+  const [cityInput, setCityInput] = useState("");
+
+  async function addItemToList(cityName) {
+    const newItem = await getCityTemperature(cityName);
+    const newList = [...temperatureList, newItem];
+    setTemperatureList(newList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Input cityInput={cityInput} setCityInput={setCityInput} />
+      <AddToListButton addItemToList={addItemToList} cityInput={cityInput} />
+      <List list={temperatureList} />
     </div>
+  );
+}
+
+function Header() {
+  return <h1>Teleportation Weather App</h1>;
+}
+
+function List({ list }) {
+  return list.map((item) => <ListItem listItem={item} />);
+}
+
+function ListItem({ listItem }) {
+  const { temperature, city } = listItem;
+
+  return (
+    <article>
+      <p>{city}:</p>
+      <p>{temperature}</p>
+    </article>
+  );
+}
+
+function AddToListButton({ addItemToList, cityInput }) {
+  return <button onClick={() => addItemToList(cityInput)}>Add To List</button>;
+}
+
+function Input({ cityInput, setCityInput }) {
+  return (
+    <input
+      value={cityInput}
+      onChange={(event) => setCityInput(event.target.value)}
+    />
   );
 }
 
